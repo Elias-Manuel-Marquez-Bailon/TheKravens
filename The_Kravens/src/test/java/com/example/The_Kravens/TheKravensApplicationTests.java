ef850f69328f71f6220992688ca222fc229908ea
Matt-Elias <emb6117@gmail.com>
2025-01-08 17:54:52 -0600

    Primer commit del repo

---
+++ The_Kravens/src/test/java/com/example/The_Kravens/TheKravensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.The_Kravens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheKravensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
